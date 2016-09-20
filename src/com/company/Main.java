8638dd9066e43b13251e066eb715c8caf98e4785
Troy <tward4@tulane.edu>
2016-09-20 09:06:49 -0400

    Inventory of items with hashmap

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -21,27 +21,27 @@ public class Main {
             }
 
 
-//            Item item = new Item("bow", 1);
-//            Item item2 = new Item("arrows", 15);
-//            Item item3 = new Item("people", 10);
-//            item.getName();
-//            item.getQuantity();
-//            items.add(item);
-//            item2.getName();
-//            item2.getQuantity();
-//            items.add(item2);
-//            item3.getName();
-//            item3.getQuantity();
-//            items.add(item3);
-//            String selectedItemName = items.get(0).getName();
-//            int quantityOfItem = items.get(0).getQuantity();
-//            String selectedItemName2 = items.get(1).getName();
-//            int quantityOfItem2 = items.get(1).getQuantity();
-//            String selectedItemName3 = items.get(2).getName();
-//            int quantityOfItem3 = items.get(2).getQuantity();
-//            System.out.printf("1. [%s] %s\n", quantityOfItem, selectedItemName);
-//            System.out.printf("2. [%s] %s\n", quantityOfItem2, selectedItemName2);
-//            System.out.printf("3. [%s] %s\n", quantityOfItem3, selectedItemName3);
+            Item item = new Item("bow", 1);
+            Item item2 = new Item("arrows", 15);
+            Item item3 = new Item("people", 10);
+            item.getName();
+            item.getQuantity();
+            items.add(item);
+            item2.getName();
+            item2.getQuantity();
+            items.add(item2);
+            item3.getName();
+            item3.getQuantity();
+            items.add(item3);
+            String selectedItemName = items.get(0).getName();
+            int quantityOfItem = items.get(0).getQuantity();
+            String selectedItemName2 = items.get(1).getName();
+            int quantityOfItem2 = items.get(1).getQuantity();
+            String selectedItemName3 = items.get(2).getName();
+            int quantityOfItem3 = items.get(2).getQuantity();
+            System.out.printf("1. [%s] %s\n", quantityOfItem, selectedItemName);
+            System.out.printf("2. [%s] %s\n", quantityOfItem2, selectedItemName2);
+            System.out.printf("3. [%s] %s\n", quantityOfItem3, selectedItemName3);
 
             boolean keepRunning = true;
             while (keepRunning) {
@@ -69,7 +69,8 @@ public class Main {
                         itemNumber = Integer.valueOf(scanner.nextLine());
                         System.out.println("Enter new quantity");
                         int newQuantity = Integer.valueOf(scanner.nextLine());
-                        Item item6 = new Item(itemNumber, newQuantity)
+                        Item item6 = new Item(itemName, newQuantity);
+                        items.add(item6);
 
 
 //                        if (items.contains(itemNumber)) {
